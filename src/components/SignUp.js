b9157ae18c84dd21bf6329dba0f45caf0a8a621d
natansevero <natanseverolp@gmail.com>
2018-09-05 22:25:54 -0300

    definindo formulário do componente de Sign Up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-export default () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import * as routes from '../constants/routes'
+
+const SignUpPage = () => (
+  <div>
+    <h1>Sign Up</h1>
+    <SignUpForm />
+  </div>
+)
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null
+}
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { ...INITIAL_STATE }
+  }
+
+  onSubmit = (event) => {
+
+  }
+
+  render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.setState(byPropKey('username', event.target.value))}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button type="submit">
+          Sign Up
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    )
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account?
+    {' '}
+    <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
+)
+
+export default SignUpPage
+
+export {
+  SignUpForm,
+  SignUpLink
 }
